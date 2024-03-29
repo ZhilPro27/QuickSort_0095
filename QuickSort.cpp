f8b7a0dc9465b3c1407630e79583454f3cd18863
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-29 13:35:37 +0700

    Membuat perulangan untuk input yang menentukan panjang array pada prosedur input

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -8,5 +8,13 @@ int mov_count = 0;	// jumlah pemindahan data
 int n;
 
 void input() { // Prodedur input
-
+	while (true) {
+		cout << "Masukkan panjang elemen array: ";	//Perulangan untuk input yang 	
+		cin >> n;									//menentukan panjang array
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+			cout << "Silahkan coba lagi" << endl;
+	}
 }
