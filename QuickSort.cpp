249296ca9752f990b6fa3bc5ebe304cfa0faeabd
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-29 13:54:55 +0700

    Membuat algoritma quick short step 8 pada prosedur quick short

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -53,6 +53,8 @@ void q_short(int low, int high) {	//Prosedur quick short
 		cmp_count++;
 		//Mencari elemen yang lebih kecil atau sama dengan pivot
 		while ((arr[j] > pivot) && (j >= low)) {	//step 7
-
+			j--;	//step 8
+			cmp_count++;
 		}
+		cmp_count++;
 }
