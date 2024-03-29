729145f817b2368f4379528f5aa24df02e9ab14d
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-29 13:29:57 +0700

    Deklarasi variable global

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+
+//Array bertipe integer untuk menyimpan nilai
+int arr[20];
+int cmp_count = 0;	// jumlah perulangan
+int mov_count = 0;	// jumlah pemindahan data
+int n;
